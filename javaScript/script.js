f8227cea308aed33d4d0ad925f6dcf0fce177e8c
barrosraffa <rafaellaponci@gmail.com>
2021-09-08 22:18:26 -0300

    btn select

---
+++ javaScript/script.js
@@ -0,0 +1,37 @@
+let elementoButton = document.querySelector("#botaoSemClick");
+//estado inicial da imagem é false, sem cor
+let estadoImagem = false; 
+let estadoBotao = false;
+elementoButton.addEventListener('click', trocaImagem);
+elementoButton.addEventListener('click', TrocaCorBotao);
+
+// se não tiver clicado no botão não mude a img se clicar muda
+
+    function trocaImagem(){
+        console.log('help')
+        let elementoImagem = document.getElementById("trocar-img")
+        if(!estadoImagem){
+            elementoImagem.src="ice-cream-click.png"
+            estadoImagem = true;
+        }
+        else {
+            elementoImagem.src="./ice-cream.png"
+            estadoImagem = false;
+        }
+    }
+
+    function TrocaCorBotao (){
+        let elementoButton = document.getElementById("botaoSemClick")
+        if(!estadoBotao){
+            console.log('botao')
+            elementoButton.classList.add('botao-com-click')
+            estadoBotao = true;
+        }
+        else {
+            elementoButton.classList.remove('botao-com-click')
+            estadoBotao = false;
+        }
+    }
+
+
+ 
